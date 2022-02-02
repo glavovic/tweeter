4542a84900d23a4d9fc63592b64d3ad846afec6f
Armin Glavovic <glavovic.armin@gmail.com>
2022-02-02 05:19:56 +0000

    added character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $('textarea').on("input", (function () {
+    const currentLenght = $(this).val().length;
+    const charCounter = 140 - currentLenght;
+    const limit = $('.counter').html(charCounter);
+
+    if (charCounter < 0) {
+      limit.css({"color" : "red"});
+    } else {
+      limit.css({"color" : "#545149"});
+    };
+  }));
+});
